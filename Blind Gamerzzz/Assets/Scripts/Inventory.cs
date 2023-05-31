4e2f139f02bb6445c33de64b7bb845429bb23ac4
Sonaf <stef.argeitis@gmail.com>
2023-05-31 23:48:36 +0200

    Updated Objective List
    Updated the Objective List to work with the new inventory system that was just implemented

--- Blind Gamerzzz/Assets/Scripts/Inventory.cs
+++ Blind Gamerzzz/Assets/Scripts/Inventory.cs
@@ -15,17 +15,28 @@ public class Inventory : MonoBehaviour
     [SerializeField] private GameObject List_UI;
     [SerializeField] private GameObject Work1_Arrow;
     public Text Component_Obj_UI;
+    public Text Component_Obj_UI2;
+    public Text Component_Obj_UI3;
+    public LightBulb bulb;
 
     // Update is called once per frame
     void Update()
     {
+        if (bulb.secondBulb){
+            Component_Obj_UI3.color = new Color (Component_Obj_UI.color.r, Component_Obj_UI.color.g, Component_Obj_UI.color.b, 0.3f);
+        }
+
         if (hasList){
             List_UI.SetActive(true);
         }
 
-        if (hasLig_1 && hasLig_2 && hasLig_3){
+        if (!bulb.firstBulb){
+            Component_Obj_UI2.color = new Color (Component_Obj_UI.color.r, Component_Obj_UI.color.g, Component_Obj_UI.color.b, 0.3f);
+        }
+
+        if (bulb.allMatsCollected){
             Component_Obj_UI.color = new Color (Component_Obj_UI.color.r, Component_Obj_UI.color.g, Component_Obj_UI.color.b, 0.3f);
-            Work1_Arrow.SetActive(true);
+            //Work1_Arrow.SetActive(true);
             all_lig_comp = true;
         }
     }
